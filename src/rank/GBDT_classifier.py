e21a63fdb332f0e45cc7e56500ad80e6436d0f37
qianchao <qianchaowqc@163.com>
2021-12-10 04:17:20 +0800

    update

--- src/rank/GBDT_classifier.py
+++ src/rank/GBDT_classifier.py
@@ -44,53 +44,50 @@ def lgb_classifier_main(trn_final_df, tst_final_df, save_path):
                 "item_sim_3", "content_sim_1", "content_sim_2", "content_sim_3", "w2v_sim_1", "w2v_sim_2",
                 "w2v_sim_3", "user_activate"]
 
-    # 五折交叉验证，并将中间结果保存用于staking
+
     for n_fold, valid_user in enumerate(user_set):
         train_idx = trn_df[~trn_df['user_id'].isin(valid_user)]  # add slide user
         valid_idx = trn_df[trn_df['user_id'].isin(valid_user)]
 
-        # 模型及参数的定义
+        # Define model
         lgb_Classfication = lgb.LGBMClassifier(boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
                                                max_depth=-1, n_estimators=100, subsample=0.7, colsample_bytree=0.7,
                                                subsample_freq=1,
                                                learning_rate=0.01, min_child_weight=50, random_state=2018, n_jobs=16,
                                                verbose=10)
-        # 训练模型
+        # train model
         lgb_Classfication.fit(train_idx[lgb_cols], train_idx['label'], eval_set=[(valid_idx[lgb_cols], valid_idx['label'])],
                               eval_metric=['auc', ], early_stopping_rounds=50, )
 
-        # 预测验证集结果
+
         valid_idx['pred_score'] = lgb_Classfication.predict_proba(valid_idx[lgb_cols],
                                                                   num_iteration=lgb_Classfication.best_iteration_)[:, 1]
 
-        # 对输出结果进行归一化 分类模型输出的值本身就是一个概率值不需要进行归一化
-        # valid_idx['pred_score'] = valid_idx[['pred_score']].transform(lambda x: norm_sim(x))
+
 
         valid_idx.sort_values(by=['user_id', 'pred_score'])
         valid_idx['pred_rank'] = valid_idx.groupby(['user_id'])['pred_score'].rank(ascending=False, method='first')
 
-        # 将验证集的预测结果放到一个列表中，后面进行拼接
+
         score_list.append(valid_idx[['user_id', 'sim_item', 'pred_score', 'pred_rank']])
 
-        # 如果是线上测试，需要计算每次交叉验证的结果相加，最后求平均
 
         sub_preds += lgb_Classfication.predict_proba(tst_final_df[lgb_cols],
                                                      num_iteration=lgb_Classfication.best_iteration_)[:, 1]
 
     score_df_ = pd.concat(score_list, axis=0)
     score_df = score_df.merge(score_df_, how='left', on=['user_id', 'sim_item'])
-    # 保存训练集交叉验证产生的新特征
+
     score_df[['user_id', 'sim_item', 'pred_score', 'pred_rank', 'label']].to_csv(
         save_path + 'trn_lgb_cls_feats.csv', index=False)
 
-    # 测试集的预测结果，多次交叉验证求平均,将预测的score和对应的rank特征保存，可以用于后面的staking，这里还可以构造其他更多的特征
+
     tst_final_df['pred_score'] = sub_preds / k_fold
     tst_final_df['pred_score'] = tst_final_df['pred_score'].transform(lambda x: norm_sim(x))
     tst_final_df.sort_values(by=['user_id', 'pred_score'])
     tst_final_df['pred_rank'] = tst_final_df.groupby(['user_id'])[
         'pred_score'].rank(ascending=False, method='first')
 
-    # 保存测试集交叉验证的新特征
     tst_final_df[['user_id', 'sim_item', 'pred_score', 'pred_rank']].to_csv(
         save_path + 'tst_lgb_cls_feats.csv', index=False)
     
--- src/rank/GBDT_rank.py
+++ src/rank/GBDT_rank.py
@@ -44,49 +44,45 @@ def lgb_main(trn_final_df, tst_final_df, save_path):
     score_df = trn_df[['user_id', 'sim_item', 'label']]
     sub_preds = np.zeros(tst_final_df.shape[0])
 
-    # 五折交叉验证，并将中间结果保存用于staking
     for n_fold, valid_user in enumerate(user_set):
         train_idx = trn_df[~trn_df['user_id'].isin(valid_user)]  # add slide user
         valid_idx = trn_df[trn_df['user_id'].isin(valid_user)]
 
-        # 训练集与验证集的用户分组
         train_idx.sort_values(by=['user_id'], inplace=True)
         g_train = train_idx.groupby(['user_id'], as_index=False).count()["label"].values
 
         valid_idx.sort_values(by=['user_id'], inplace=True)
         g_val = valid_idx.groupby(['user_id'], as_index=False).count()["label"].values
 
-        # 定义模型
+        # Define model
         lgb_ranker = lgb.LGBMRanker(boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
                                     max_depth=-1, n_estimators=100, subsample=0.7, colsample_bytree=0.7,
                                     subsample_freq=1,
                                     learning_rate=0.01, min_child_weight=50, random_state=2018, n_jobs=16)
-        # 训练模型
+        # train model
         lgb_ranker.fit(train_idx[lgb_cols], train_idx['label'], group=g_train,
                        eval_set=[(valid_idx[lgb_cols], valid_idx['label'])], eval_group=[g_val],
                        eval_at=[10, 20, 30, 40, 50], eval_metric=['ndcg', ], early_stopping_rounds=50, )
 
-        # 预测验证集结果
+
         valid_idx['pred_score'] = lgb_ranker.predict(valid_idx[lgb_cols], num_iteration=lgb_ranker.best_iteration_)
 
-        # 对输出结果进行归一化
         valid_idx['pred_score'] = valid_idx[['pred_score']].transform(lambda x: norm_sim(x))
 
         valid_idx.sort_values(by=['user_id', 'pred_score'])
         valid_idx['pred_rank'] = valid_idx.groupby(['user_id'])['pred_score'].rank(ascending=False, method='first')
 
-        # 将验证集的预测结果放到一个列表中，后面进行拼接
+
         score_list.append(valid_idx[['user_id', 'sim_item', 'pred_score', 'pred_rank']])
 
         sub_preds += lgb_ranker.predict(tst_final_df[lgb_cols], lgb_ranker.best_iteration_)
 
     score_df_ = pd.concat(score_list, axis=0)
     score_df = score_df.merge(score_df_, how='left', on=['user_id', 'sim_item'])
-    # 保存训练集交叉验证产生的新特征
+
     score_df[['user_id', 'sim_item', 'pred_score', 'pred_rank', 'label']].to_csv(
         save_path + 'trn_lgb_ranker_feats.csv', index=False)
 
-    # 测试集的预测结果，多次交叉验证求平均,将预测的score和对应的rank特征保存，可以用于后面的staking，这里还可以构造其他更多的特征
     tst_final_df['pred_score'] = sub_preds / k_fold
     tst_final_df['pred_score'] = tst_final_df['pred_score'].transform(
         lambda x: norm_sim(x))
@@ -94,7 +90,6 @@ def lgb_main(trn_final_df, tst_final_df, save_path):
     tst_final_df['pred_rank'] = tst_final_df.groupby(['user_id'])[
         'pred_score'].rank(ascending=False, method='first')
 
-    # 保存测试集交叉验证的新特征
     tst_final_df[['user_id', 'sim_item', 'pred_score', 'pred_rank']].to_csv(
         save_path + 'tst_lgb_ranker_feats.csv', index=False)
         
